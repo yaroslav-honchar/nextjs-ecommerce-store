ee7ab58888a2b3ef0c56eb0d815707fe48522877
Yaroslav Honchar <yaroslav.honchar.1@gmail.com>
2024-09-04 10:27:12 +0300

    feat: added suspense to cart page

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -1,5 +1,12 @@
+"use client"
+
+import { Suspense } from "react"
 import { CartTable } from "@/components/cart-table/cart-table"
 
 export default function CartPage() {
-  return <CartTable />
+  return (
+    <Suspense>
+      <CartTable />
+    </Suspense>
+  )
 }
